91ba483ad99fc76e07e4613b80534421342c8287
Mastergiant17 <minkemaw@my.easternct.edu>
2024-03-25 13:27:19 -0400

    Update hello.java

--- hello.java
+++ hello.java
@@ -5,10 +5,11 @@ public class hello {
     public static void main(String [] args) {
 
        // initialize variables
-       String firstName = "Alex";
+       String firstName = "William";
 
        // delete this line
-       System.out.println("Modify this code");
+       System.out.println("Hello, I am");
+       System.out.println(firstName);
 	
        // Using the value of the variable above, output a message in the format, "Hello, I am Alex".
 
